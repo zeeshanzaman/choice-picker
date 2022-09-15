fdde22bac9c26bcf141c982b696684dc4d84e9dd
zeeshanzaman <88842812+zeeshanzaman@users.noreply.github.com>
2022-09-15 11:04:58 +0500

    create if condition for random select

--- custom.js
+++ custom.js
@@ -5,6 +5,14 @@ textarea.focus()
 
 textarea.addEventListener('keyup', (e) => {
     createTags(e.target.value)
+
+    if (e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value = ''
+        }, 10)
+
+        randomSelect()
+    }
 })
 
 function createTags(input) {
